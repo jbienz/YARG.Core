7eeabc6e1ae6b91873e3e7502d02aaf53bc281d6
RileyTheFox <jacobchambers@hotmail.co.uk>
2024-06-21 15:08:51 +0100

    Remove IBinarySerializable from Engine Stats classes

--- YARG.Core/Engine/Guitar/GuitarStats.cs
+++ YARG.Core/Engine/Guitar/GuitarStats.cs
@@ -1,6 +1,4 @@
-﻿using System.IO;
-
-namespace YARG.Core.Engine.Guitar
+﻿namespace YARG.Core.Engine.Guitar
 {
     public class GuitarStats : BaseStats
     {
@@ -51,28 +49,6 @@ namespace YARG.Core.Engine.Guitar
             GhostInputs = 0;
             WhammyTicks = 0;
             SustainScore = 0;
-        }
-
-        public override void Serialize(BinaryWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.Write(Overstrums);
-            writer.Write(HoposStrummed);
-            writer.Write(GhostInputs);
-            writer.Write(WhammyTicks);
-            writer.Write(SustainScore);
-        }
-
-        public override void Deserialize(BinaryReader reader, int version = 0)
-        {
-            base.Deserialize(reader, version);
-
-            Overstrums = reader.ReadInt32();
-            HoposStrummed = reader.ReadInt32();
-            GhostInputs = reader.ReadInt32();
-            WhammyTicks = reader.ReadUInt32();
-            SustainScore = reader.ReadInt32();
         }
     }
 }
